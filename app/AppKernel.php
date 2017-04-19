ac5d0da36d397bef96a5f12de69483e96197a29f
Douglas Metthey <d.metthey@gmail.com>
2017-04-19 15:51:17 +0200

    Installation thème AdminLte

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AuthBundle\AuthBundle(),
             new \FOS\UserBundle\FOSUserBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
