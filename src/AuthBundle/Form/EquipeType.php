2b46d7617d4a55100a7a6e2d893de350fa39456c
Douglas Metthey <d.metthey@gmail.com>
2017-04-20 14:39:45 +0200

    génération CrUD sur USER et EQUIPE

---
+++ src/AuthBundle/Form/EquipeType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AuthBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EquipeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom')->add('createdAt')->add('modules');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AuthBundle\Entity\Equipe'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'authbundle_equipe';
+    }
+
+
+}
---
+++ src/AuthBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AuthBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom')->add('prenom')->add('createdAt')->add('activeAt')->add('activeUntil')->add('createdBy');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AuthBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'authbundle_user';
+    }
+
+
+}
