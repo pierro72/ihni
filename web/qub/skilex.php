c77053d22ac4de3032180f12d1dde4a72dbe03e5
Douglas Metthey <d.metthey@gmail.com>
2017-07-04 12:20:23 +0200

    fake API pour modules

--- web/qub/skilex.php
+++ web/qub/skilex.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Sylia</title>
+    <title>Skilex</title>
 
     <!-- Bootstrap -->
     <link href="../assets/vendor/AdminLTE/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -258,12 +258,12 @@
                 })
                 ;
 
-                
+
                 data.users.forEach(function (teamRole) {
                     $('#equipes_role').append(
                         "<tr>" +
-                            "<td>" + teamRole.user.prenom + " " + teamRole.user.nom + "</td>" +
-                            "<td>" + teamRole.role + "</td>" +
+                        "<td>" + teamRole.user.prenom + " " + teamRole.user.nom + "</td>" +
+                        "<td>" + teamRole.role + "</td>" +
                         "</tr>"
                     )
                 })
