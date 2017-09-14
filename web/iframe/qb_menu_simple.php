2e501e23078ac48c7220d1a52ff7832bc648db21
Douglas Metthey <d.metthey@gmail.com>
2017-09-14 09:59:12 +0200

    changement version

--- web/iframe/qb_menu_simple.php
+++ web/iframe/qb_menu_simple.php
@@ -39,8 +39,8 @@
                 </div>
 
                 <ol class="p_breadcrumb">
-                    <li><a href="/qub" target="_top">IHNI</a></li>
-                    <li><a href="/qub/team_table/" target="_top">Choix d'équipe</a></li>
+                    <li><a href="/" target="_top">IHNI</a></li>
+                    <li><a href="/team_table/" target="_top">Choix d'équipe</a></li>
                     <li><a href="#" target="_top">Choix module</a></li>
                 </ol>
 
@@ -49,7 +49,7 @@
 
                     <!-- Bienvenue + nom de l'utilisateur -->
                     <ul class="nav navbar-nav">
-                        <li><a href="/qub/ihni/user" target="_top">IHNI</a> </li>
+                        <li><a href="/ihni/user" target="_top">IHNI</a> </li>
                         <li><a href="/logout" target="_top">Déconnection</a> </li>
                     </ul>
 
